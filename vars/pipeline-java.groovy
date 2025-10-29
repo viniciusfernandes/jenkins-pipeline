9b5887c6e562e9b7a134a3c2a206e9bb15291933
vinicius <viniciussf@hotmail.com>
2025-10-29 17:09:15 -0300

    Added pipeline-java.groovy

---
+++ vars/pipeline-java.groovy
@@ -0,0 +1,38 @@
+def call(Map config = [:]) {
+    pipeline {
+        agent any
+        stages {
+            stage('Checkout') {
+                steps {
+                    git branch: config.branch ?: 'main', url: config.repo
+                }
+            }
+            stage('Build') {
+                steps {
+                    sh './mvnw clean compile -B'
+                }
+            }
+            stage('Test') {
+                steps {
+                    sh './mvnw test -B'
+                }
+                post {
+                    always {
+                        junit '**/target/surefire-reports/*.xml'
+                    }
+                }
+            }
+            stage('Package') {
+                steps {
+                    sh './mvnw package -DskipTests -B'
+                    archiveArtifacts artifacts: 'target/*.jar', fingerprint: true
+                }
+            }
+            stage('Docker Build') {
+                steps {
+                    sh "docker build -t ${config.image ?: 'myapp:latest'} ."
+                }
+            }
+        }
+    }
+}
